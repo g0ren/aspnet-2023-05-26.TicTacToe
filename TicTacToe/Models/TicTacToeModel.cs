3fb030c8c01f4a7cd0b6ac6dcfdb0ad4f7af1b15
g0ren <g0ren@mail.ru>
2023-06-01 11:43:31 +0300

    Initial commit

---
+++ TicTacToe/Models/TicTacToeModel.cs
@@ -0,0 +1,27 @@
+﻿namespace TicTacToe.Models;
+
+public class TicTacToeModel
+{
+    public List<char> Field { get; }
+        public TicTacToeModel()
+        {
+            Random random = new Random();
+            Field = new List<char>();
+            for(int i = 0; i < 9; i++)
+            {
+                int val = random.Next(3);
+                if (val == 0)
+                {
+                    Field.Add(' '); 
+                }
+                else if (val == 1)
+                {
+                    Field.Add('X');
+                }
+                else
+                {
+                    Field.Add('0');
+                }
+            }
+        }
+    }
